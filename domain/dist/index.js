9c1117fc5474e5f7860c63b2563ae592229e82e9
sgamidb <36730095+sgamidb@users.noreply.github.com>
2023-07-11 10:24:10 +0200

    remove dist

--- domain/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addAgeOfCaptain = void 0;
-const shared_lib_1 = require("@test-sgamid/shared-lib");
-function addAgeOfCaptain(ageOfCaptain, numberOfBarrels) {
-    return (0, shared_lib_1.sum)(ageOfCaptain, numberOfBarrels);
-}
-exports.addAgeOfCaptain = addAgeOfCaptain;
